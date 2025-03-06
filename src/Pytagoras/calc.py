e5e4527aa0d3aed7eedccb2a1a9db7b538b0ae1e
Grasiele Romanzini-Bezerra <bezerragrasiele@gmail.com>
2025-03-06 11:56:13 +0000

    Write functions and set up test template

---
+++ src/Pytagoras/calc.py
@@ -0,0 +1,24 @@
+# Module to calculate the hypotenuse of right-angle triangle
+#will have two functions, one to get the squares of the opp and adj
+#One to calculate the square root of those added together
+#a**2 + b**2 = c**c
+import numpy as np
+
+#Function to get the squares of opp and adj
+#Inputs/args: opp and adj (a and b)
+#Outputs/returns: a**2 and b**2
+def squares (opp, adj):
+    opp_sq = opp**2
+    adj_sq = adj**2
+    return opp_sq, adj_sq
+
+
+
+#Function to calculate the sqrt of those added
+#Inputs/args: opp and adj squared (a**2 and b**2)
+#Output/returns: hypotenuse (c)
+def hypot (opp_sq, adj_sq):
+    sum_a_a = opp_sq + adj_sq
+    hypot_sqrt = np.sqrt(sum_a_a)
+    return hypot_sqrt
+    
